f87f8aa4413777f26f9d431255953954646ef0c8
Champ4618 <bhku1997@gmail.com>
2020-02-14 15:23:44 +0530

    14-02-feb/3:23pm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,18 @@ import { HeaderComponent } from './header/header.component';
 import { AdminComponent } from './admin/admin.component';
 import { ContactComponent } from './contact/contact.component';
 import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { DemoComponent } from './demo/demo.component';
+import { Demo1Component } from './demo1/demo1.component';
 
 
 const routes: Routes = [
   {path: 'header', component: HeaderComponent},
   {path: 'admin', component: AdminComponent},
   {path: 'contact', component: ContactComponent},
-  {path: 'adminhome', component: AdminhomeComponent}
+  {path: 'adminhome', component: AdminhomeComponent},
+  {path: 'demo', component: DemoComponent},
+  {path: 'demo1', component: Demo1Component}
+
 
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,9 @@ import { TeacherComponent } from './teacher/teacher.component';
 import { ContactComponent } from './contact/contact.component';
 import {MatButtonModule} from '@angular/material/button';
 import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { DemoComponent } from './demo/demo.component';
+import { Demo1Component } from './demo1/demo1.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,13 +22,17 @@ import { AdminhomeComponent } from './adminhome/adminhome.component';
     AdminComponent,
     TeacherComponent,
     ContactComponent,
-    AdminhomeComponent
+    AdminhomeComponent,
+    DemoComponent,
+    Demo1Component,
   ],
   imports: [
     BrowserModule,
     MatButtonModule,
     MatInputModule,
-    AppRoutingModule
+    MatTabsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
